12ab5f24510686ec4a7a54c17e12539e31aed241
Ryan@RCNP <ryantang@rcnp.osaka-u.ac.jp>
2015-10-10 15:37:00 +0900

    modified cpp/RK4/RK4.f

--- cpp/RK4/RK4.h
+++ cpp/RK4/RK4.h
@@ -18,15 +18,17 @@ double G(double r, double u, double T, double L, double Pot){
   return (2*mp/pow(hbarc,2))*(Pot-T)*u + L*(L+1)*u/pow(r,2);
 } 
 
-int RKfouth (double KE, int L, int nStep, double dr, float V0, float R0, float a0){
+int RKfouth (const double KE, int L, int nStep, double dr, float V0, float R0, float a0){
 
   // output is a file "RK4.dat"
 
+  const double scale = abs(V0);
   double rStart = 0.00001;
   double rEnd = rStart + dr * nStep;
 
   printf("-------------- inputs\n");
   printf(" KE: %6.2f MeV, L: %2d \n", KE, L);
+  printf(" V0: %6.2f MeV, R0: %6.2f fm, a0: %6.2f fm \n", V0, R0, a0);
   printf(" start: %7.5f fm, dr:%5.3f, end: %10.5f fm, nStep: %3d \n", rStart, dr, rEnd, nStep);
  
   const int nStep1 = nStep + 1 ;
@@ -38,10 +40,13 @@ int RKfouth (double KE, int L, int nStep, double dr, float V0, float R0, float a
   FILE * paraOut;
   paraOut = fopen ("RK4.dat", "w");
   fprintf(paraOut, "# KE: %6.2f MeV, L: %2d \n", KE, L);
+  fprintf(paraOut, "# V0: %6.2f MeV, R0: %6.2f fm, a0: %6.2f fm\n", V0, R0, a0);
   fprintf(paraOut, "# start: %7.5f fm, dr:%5.3f, end: %10.5f fm, nStep: %3d \n", rStart, dr, rEnd, nStep);
-  fprintf(paraOut, "#%12s %12s %12s %12s %12s %12s\n", "r", "Pot", "u", "du/dr", "R", "dR/dr");
-  fprintf(paraOut, " %12.3E %12.3E %12.3E %12.3E %12.3E %12.3E\n", rStart, Pot(rStart, V0, R0, a0), SolU[0][0], SolU[0][1], SolU[0][0]/rStart, SolU[0][1]/rStart - SolU[0][0]/pow(rStart,2));
-  
+  fprintf(paraOut, "#%12s %12s %9s%-3.0f %12s %12s %12s %12s %9s%-3.0f\n", "r", "Pot/100", "(Pot+LS)/", scale, "u", "du/dr", "R", "dR/dr", "KE/", scale);
+  fprintf(paraOut, "#----------\n");
+  fprintf(paraOut, " %12.3E %12.3E %12.3E %12.3E %12.3E %12.3E %12.3E %12.3E\n", rStart, Pot(rStart, V0, R0, a0)/scale, 0, SolU[0][0], SolU[0][1], SolU[0][0]/rStart, SolU[0][1]/rStart - SolU[0][0]/pow(rStart,2), 0.0);
+
+  double maxu = 0;  
   for(int i = 0; i <= nStep; i++){
     double r = rStart + i * dr;
     double pot = Pot(r, V0, R0, a0);
@@ -67,10 +72,24 @@ int RKfouth (double KE, int L, int nStep, double dr, float V0, float R0, float a
     
     SolU[i + 1][1] = v + dv;
     SolU[i + 1][0] = u + du;
-    
-    fprintf(paraOut, " %12.3E %12.3E %12.3E %12.3E %12.3E %12.3E\n", r+dr, pot, SolU[i+1][0], SolU[i+1][1], SolU[i+1][0]/r, SolU[i+1][1]/r - SolU[i+1][0]/pow(r,2));
+
+    if ( u > maxu) maxu = u;
   }
   
+  printf("------- normalized ul \n");
+
+  for( int i = 0; i <= nStep; i++){
+    double r = rStart + i*dr;
+    double pot = Pot(r+dr,V0,R0,a0);
+    double poteff = pot + pow(hbarc/(r+dr),2)*L*(L+1)/2/mp;
+    if ( abs(poteff) > abs(V0) ) poteff = 0.0;
+    SolU[i+1][1] = SolU[i+1][1]/maxu;
+    SolU[i+1][0] = SolU[i+1][0]/maxu;
+    
+    fprintf(paraOut, " %12.3E %12.3E %12.3E %12.3E %12.3E %12.3E %12.3E %12.3E\n", r+dr, pot/scale, poteff/scale , SolU[i+1][0], SolU[i+1][1], SolU[i+1][0]/r, SolU[i+1][1]/r - SolU[i+1][0]/pow(r,2), KE/scale);
+  }
+
+  fclose(paraOut);
   printf("----------done.\n");
 
   return 0;
